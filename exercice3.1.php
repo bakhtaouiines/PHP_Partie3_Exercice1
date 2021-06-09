8cf4856f61e63e9ae725764b9fce8b88a6979169
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:31:50 +0200

    exercice 3.1 fait

---
+++ exercice3.1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 3.1</title>
+</head>
+<body>
+    <h1>Exercice 1 Partie 3 : Les boucles PHP</h1>
+        <p>
+            <?php
+            $number = 0;
+            while ($number <= 10)
+            {
+                echo $number++;
+            }
+            ?>
+        </p>
+</body>
+</html>
